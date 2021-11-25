bd58a063dd50073c350345086a718daf33b540ff
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 10:25:43 +0530

    Create loop.c

---
+++ loop.c
@@ -0,0 +1,12 @@
+// online c compiler to run c program online 
+2. #include <stdio.h>
+3. 
+4.int main ()
+5.{
+6.       int a;
+7.       printf("enter a number");
+8.       scanf("%d",&a);
+9.       for(a;a<10;a++)
+10.      printf("%d\n",a);  
+11.       return 0;     
+12.}      
