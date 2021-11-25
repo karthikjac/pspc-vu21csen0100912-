fd07e058f9d84180eb7419d5691d39f9d370c702
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 09:56:54 +0530

    Create no.c

---
+++ no.c
@@ -0,0 +1,11 @@
+1. // Online C compiler to run C program online
+2. #include <stdio.h>
+3. 
+4. int main() 
+5. {
+6 int a=2;
+7. for(a=2;a<10;++a)
+8. printf("\n %d",++a);
+9 
+10.    return 0;
+11. }
