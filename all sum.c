fef15b312c43e0dbe8a64bd5ffbd7b140ca151c0
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 09:51:03 +0530

    Create all sum.c

---
+++ all sum.c
@@ -0,0 +1,19 @@
+1. // online c compiler to run c program online 
+2. #include <stdio.h>
+3. 
+4.int main ()
+5.{
+6.       int a=20,b=10,c;
+7.        c=a+b;
+8.       printf("a+b=%d",c);
+9.       c=a-b;
+10..       printf("a-b=%d",c);
+11.      c=a*b;
+12.        printf("a*b=%d",c);
+13.      c=a/b; 
+14.      printf("a/b=%d",c);
+15.         c=a%b;
+16.         printf(remainder when a divided by b= %d",c);
+17. 
+18.       return 0;     
+19.}      
