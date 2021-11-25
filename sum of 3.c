31e7ec26a83faa6a481369f8c6f62ff447762c1a
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 09:52:55 +0530

    Create sum of 3.c

---
+++ sum of 3.c
@@ -0,0 +1,16 @@
+1. // online c compiler to run c program online 
+2. #include <stdio.h>
+3. 
+4.int main ()
+5.{
+6.       int a,b,c,d;
+7.       printf("enter the value of a");
+8.       scanf("%d",&a);
+9.       printf("enter the value of b");
+10.      scanf("%d",&b);
+11.      printf("enter the value of c"); 
+12.      scanf("%d",&c);      
+13.      d=(a+b+c)/3 
+14.      printf("the average value of a and b and c is %d",d); 
+15.       return 0;     
+16.}      
