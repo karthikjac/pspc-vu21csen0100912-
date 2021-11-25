9c257dd4ffc4e80e30706a401f11634facb5a5d5
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 09:54:21 +0530

    Create odd and even.c

---
+++ odd and even.c
@@ -0,0 +1,18 @@
+ // online c compiler to run c program online 
+2. #include <stdio.h>
+3. 
+4.int main ()
+5.{
+6.       int a;
+7.       printf("enter the value of a");
+8.       scanf("%d",&a);
+9.       if(a%2==0)
+10.       {
+11.             printf("even number"); 
+12.       }      
+13.       else
+14.       {
+15.               printf("odd number"); 
+16.       }
+17.       return 0;     
+18.}      
