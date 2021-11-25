3abb0020b6862ce1ac7bb009c38829c5482fd155
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 10:07:36 +0530

    Create minutes.c

---
+++ minutes.c
@@ -0,0 +1,10 @@
+//converting minutes to seconds
+#include <stdio.h>
+int main()
+{
+    int m,s;
+    printf("enter time in minutes");
+    scanf("%d",&m);
+    s=m*60;
+    printf("time in seconds is: %d",s);
+}
