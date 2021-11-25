f98ff36f5c0a48764e14539da11aee8d24845eff
karthikjac <95009339+karthikjac@users.noreply.github.com>
2021-11-25 09:51:51 +0530

    Create sum of 2 .c

---
+++ sum of 2 .c
@@ -0,0 +1,14 @@
+1. // online c compiler to run c program online 
+2. #include <stdio.h>
+3. 
+4.int main ()
+5.{
+6.       int a,b,c
+7.       printf("enter the value of a");
+8.       scanf("%d",&a);
+9.       printf("enter the value of b");
+10.      scanf("%d",&b);
+11.      c=a-b; 
+12.      printf("the average value of a and b is %d",%c); 
+15.       return 0;     
+16.}      
